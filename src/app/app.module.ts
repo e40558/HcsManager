271269c6fb4ceaf4bc78a8c370f6c187b51c2bdd
Anddre McCaskill <mccaskillhld@yahoo.com>
2020-02-11 11:11:36 -0600

    menu complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { FwModule } from '../fw/fw.module';
@@ -22,6 +23,7 @@ import { CountryMaintComponent } from './country-maint/country-maint.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FwModule,
     RouterModule.forRoot(appRoutes)
   ],
--- src/menus/menu-item/menu-item.component.ts
+++ src/menus/menu-item/menu-item.component.ts
@@ -1,13 +1,25 @@
 import { Component, ElementRef, HostBinding, HostListener,
-  Input, OnInit, Renderer, Renderer2 } from '@angular/core';
+  Input, OnInit, Renderer} from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { MenuItem, MenuService } from 'src/fw/services/menu.service';
+import { transition, trigger, style, animate } from '@angular/animations';
 
 
 @Component({
 selector: 'fw-menu-item',
 templateUrl: './menu-item.component.html',
-styleUrls: ['./menu-item.component.css']
+styleUrls: ['./menu-item.component.css'],
+  animations: [
+        trigger('visibilityChanged', [
+            transition(':enter', [   // :enter is alias to 'void => *'
+                style({opacity: 0}),
+                animate(1000, style({opacity: 1}))
+            ]),
+            transition(':leave', [   // :leave is alias to '* => void'
+                animate(500, style({opacity: 0}))
+            ])
+        ])
+    ]
 })
 export class MenuItemComponent implements OnInit {
 @Input() item =  null as MenuItem;  // see angular-cli issue #2034
